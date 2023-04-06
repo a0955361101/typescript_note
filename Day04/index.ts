a652764170a912c497d7ceaca787ff04acc2a3c8
Bill <a0955361101@gmail.com>
2023-04-06 17:33:08 +0800

    note

---
+++ Day04/index.ts
@@ -0,0 +1,6 @@
+// 函式型別 Function Types
+let aSimpleFunction = () => console.log('hi'); // () => void 
+// 這個函式格式 —— 它的輸入端是空的，輸出端是 void 代表的也是空值，或者代表不回傳的狀態。
+
+
+// const addition = (num1, num2) => num1 + num2; // 參數被推論為 any => 報錯
