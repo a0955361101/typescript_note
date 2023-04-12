07db2428f527f80fda6007888dbca877deeaa724
Bill <a0955361101@gmail.com>
2023-04-12 18:05:32 +0800

    note

--- Day04/index.ts
+++ Day04/index.ts
@@ -4,3 +4,22 @@ let aSimpleFunction = () => console.log('hi'); // () => void
 
 
 // const addition = (num1, num2) => num1 + num2; // 參數被推論為 any => 報錯
+
+// const addition = (param1, param2) => param1 + param2; // 被推論為 any => 報錯
+
+let addition = (param1: number, param2: number) => param1 + param2;  // => ok
+
+// let shouldBeString: string = addition(123,456) // => 類型 number 不可指派給 string
+
+let addition2 = (num1: number, num2: number): number => num1 + num2; // 如何註記輸出部分
+
+const aJSONString = '{"Hello":"World","luckyNumber":14}';
+// TS 不會鳥你的狀況
+let parsedJSON = JSON.parse(aJSONString);
+
+// 接受 TS 型別系統的擁抱
+let parsedJSON1 = JSON.parse(aJSONString) as { hello: string, luckyNumber: number; };
+let parsedJSON2 = <{ hello: string, luckyNumber: number; }>JSON.parse(aJSONString);
+let parsedJSON3: { hello: string, luckyNumber: number; } = JSON.parse(aJSONString);
+
+let parsedJSON4 = JSON.parse(aJSONString); // => any
