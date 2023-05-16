b663bb671f98c4a814d8e7fcb2958e75c8fed117
Bill <a0955361101@gmail.com>
2023-05-16 13:31:08 +0800

    note

---
+++ Day08/index.ts
@@ -0,0 +1,15 @@
+let addOp = (n1: number, n2: number) => {
+  return n1 + n2;
+}; // => let addOp:(n1: number, n2: number) => number;
+
+let subtractOp = (n1: number, n2: number) => {
+  return n1 - n2;
+}; // => let subtractOp:(n1: number, n2: number) => number;
+
+let multiplyOp = (n1: number, n2: number) => {
+  return n1 * n2;
+}; // => let multiplyOp:(n1: number, n2: number) => number;
+
+let divideOp = (n1: number, n2: number) => {
+  return n1 / n2;
+}; // => let divideOp:(n1: number, n2: number) => number;
